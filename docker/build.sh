42912c1a67b622bc034f2464fec5c73342c60a1d
neli <n.kagbe@fintechandpaymentsinstitute.com>
2025-04-11 14:34:27 +0000

    - added expired time to temp password

--- docker/build.sh
+++ docker/build.sh
@@ -1,22 +1,23 @@
-##!/bin/bash
-#
-##CREDENTIALS
-#userName=nelieli
-#repo=$userName/elteq
-#
-##IMAGE TAGS
-#base=project
-#name=chat-service
-#version=v1
-#image=$base-$name-$version
-#to_push=$repo:$image
-#
-#docker rmi $image -f
-#docker rmi $to_push -f
-#
-#DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -f docker/Dockerfile -t $image .
-#docker image tag $image $to_push
-#
-##docker login -u $userName -p
-#docker image push $to_push
-#docker logout $userName
+#!/bin/bash
+
+#CREDENTIALS
+userName=nelieli
+password=dckr_pat_G08s1YNsq0YyEAg_wBdumKF7txY
+repo=$userName/elteq
+
+#IMAGE TAGS
+base=project
+name=chat-service
+version=v1
+image=$base-$name-$version
+to_push=$repo:$image
+
+docker rmi $image -f
+docker rmi $to_push -f
+
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -f docker/Dockerfile -t $image .
+docker image tag $image $to_push
+
+docker login -u $userName -p $password
+docker image push $to_push
+docker logout $userName
