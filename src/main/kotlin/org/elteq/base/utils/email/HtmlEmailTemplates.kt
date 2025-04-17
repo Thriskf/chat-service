c6c749b24137b0f0c8db61a18bea229d8cb6d4ae
Neli <thrisk.f@gmail.com>
2025-04-17 10:29:56 +0000

    fixed email templates by adding this   <args>                    <arg>-java-parameters</arg>   </args>

--- src/main/kotlin/org/elteq/base/utils/email/HtmlEmailTemplates.kt
+++ src/main/kotlin/org/elteq/base/utils/email/HtmlEmailTemplates.kt
@@ -2,8 +2,9 @@ package org.elteq.base.utils.email
 
 import io.quarkus.qute.CheckedTemplate
 import io.quarkus.qute.TemplateInstance
+import jakarta.inject.Named
 
-@CheckedTemplate
+@CheckedTemplate(basePath = "HtmlEmailTemplates")
 object HtmlEmailTemplates {
 
 //    fun welcome(name: String): TemplateInstance = throw NotImplementedError()
@@ -15,12 +16,14 @@ object HtmlEmailTemplates {
     external fun welcome(name: String): TemplateInstance
 
     @JvmStatic
+//    external fun resetPassword(@Named("password") password: String): TemplateInstance
     external fun resetPassword(password: String): TemplateInstance
 
     @JvmStatic
+//    external fun signUp(@Named("name") name: String,
+//                        @Named("password") password: String): TemplateInstance
     external fun signUp(name: String, password: String): TemplateInstance
 
     @JvmStatic
     external fun updatePassword(name: String): TemplateInstance
-
 }
